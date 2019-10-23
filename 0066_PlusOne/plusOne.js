7dba20a5d8d339d4cf45f0794c69181c098cd257
Eason Lin <uu9924079@gmail.com>
2019-10-23 16:26:08 +0800

    add 0066

---
+++ 0066_PlusOne/plusOne.js
@@ -0,0 +1,52 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+
+/* 
+https://leetcode.com/problems/plus-one/submissions/
+
+解法:
+1. 判斷相加後是否需進位
+2. 目前位數 = 目前位數+前面是否進位
+3. digits個位數+1
+4. 如果目前這個位數等於10(因為只+1，所以不會超過10)，進位
+5. 最後如果有進位就放到索引0
+*/
+
+var plusOne = function (digits) {
+  // 1. carry用來判斷相加後是否需進位
+  let carry = 0
+
+  for (let i = digits.length - 1; i >= 0; i--) {
+    // 2. 目前位數 = 目前位數+前面是否進位
+    digits[i] = digits[i] + carry
+
+    // 3. digits個位數+1
+    if (i === digits.length - 1) {
+      digits[i] = digits[i] + 1
+    }
+
+    // 4. 如果目前這個位數等於10(因為只+1，所以不會超過10)，進位
+    if (digits[i] === 10) {
+      digits[i] = 0
+      carry = 1
+    } else {
+      carry = 0
+    }
+  }
+
+  // 5. 最後如果有進位就放到索引0
+  if (carry == 1) {
+    digits.unshift(carry)
+  }
+
+  return digits
+}
+
+console.log(plusOne([])) // output: null
+console.log(plusOne([1, 9, 9])) // output: [2, 0, 0]
+console.log(plusOne([9, 9])) // output: [1, 0, 0]
+console.log(plusOne([4, 3, 2, 1])) // output: [4, 3, 2, 2]
+console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]))
+// output: [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,4]
