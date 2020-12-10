dcf84d51c7fc0d18e785bd2422d39e90c24d43f9
Eason Lin <uu9924079@gmail.com>
2020-12-10 16:34:44 +0800

    add third solution

--- 0442_FindAllDuplicatesInAnArray.js
+++ 0442_FindAllDuplicatesInAnArray.js
@@ -7,18 +7,18 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var findDuplicates = function (nums) {
-  nums = nums.sort((a, b) => a - b);
-  let i = 0;
-  while (i < nums.length) {
-    if (nums[i] !== nums[i + 1]) {
-      nums.splice(i, 1);
-    } else {
-      i++;
-    }
-  }
-  return nums;
-};
+// var findDuplicates = function (nums) {
+//   nums = nums.sort((a, b) => a - b);
+//   let i = 0;
+//   while (i < nums.length) {
+//     if (nums[i] !== nums[i + 1]) {
+//       nums.splice(i, 1);
+//     } else {
+//       i++;
+//     }
+//   }
+//   return nums;
+// };
 // 1056ms(7.52%) 49.7MB(41.37%)
 // 思路：
 // 先進行排序
@@ -29,4 +29,35 @@ var findDuplicates = function (nums) {
 // 　　否則
 // 　　　　索引 + 1
 // 回傳陣列
+
+// var findDuplicates = function (nums) {
+//   return nums.sort().filter((x, i) => {
+//     return x === nums[i - 1];
+//   });
+// };
+// 思路：
+// 先進行排序
+// 遍歷並找出重複的部分
+
+var findDuplicates = function (nums) {
+  const res = [];
+  for (let i = 0; i < nums.length; i++) {
+    const index = Math.abs(nums[i]) - 1;
+    if (nums[index] < 0) {
+      res.push(Math.abs(index + 1));
+    }
+    nums[index] = -nums[index];
+  }
+  return res;
+};
+// 解法：
+// （參考網站上最佳解）
+// 宣告 res 為空陣列
+// 遍歷 nums
+// 　　宣告 index 為目前被遍歷到的絕對值 - 1
+// 　　如果 nums[index] 小於 0
+// 　　　　就把 index + 1 加入 res
+// 　　把 nums[index] 改為 -nums[index]
+//
+// 116ms(70.09%) 46.2MB(88.89%)
 console.log(findDuplicates([4, 3, 2, 7, 8, 2, 3, 1])); // [2,3]
