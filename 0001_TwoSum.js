c4ce25464c9e1d85cb1348a422a635e387bebe15
Eason Lin <uu9924079@gmail.com>
2020-12-12 09:14:45 +0800

    add solutions

--- 0001_TwoSum.js
+++ 0001_TwoSum.js
@@ -7,15 +7,42 @@ solution:
 
 */
 
+// var twoSum = function (nums, target) {
+//   // 1. iterate numbers in the array
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       // 2. try every combination of each number
+//       if (nums[i] + nums[j] === target) {
+//         // 3. once there's an answer, return it
+//         return [i, j]
+//       }
+//     }
+//   }
+// };
+
+/**
+ * 解法：
+ * 先宣告 map 用來存鍵值對，鍵是計算過的數字，值是索引
+ * 遍歷 nums
+ * 　　把現在索引值宣告成 v
+ * 　　如果目標 target 扣除 v 是找得到的
+ * 　　　　就回傳找到的鍵的索引以及目前的索引
+ * 　　否則
+ * 　　　　把 v 的值寫為鍵，索引寫為值
+ * 這題本來是用兩層暴力解，後來發現網路上有人用 map 去解，就吸收起來，因為 map 中的鍵都是已經算過的數字，如果有找到該鍵表示相加等於目標，就能拿到其索引並回傳
+ */
+
 var twoSum = function (nums, target) {
-  // 1. iterate numbers in the array
+  const map = {};
   for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      // 2. try every combination of each number
-      if (nums[i] + nums[j] === target) {
-        // 3. once there's an answer, return it
-        return [i, j]
-      }
+    const v = nums[i];
+    if (map[target - v] >= 0) {
+      return [map[target - v], i];
+    } else {
+      map[v] = i;
     }
   }
 };
+
+console.log(twoSum([2, 7, 11, 15], 9));
+// console.log(twoSum([2, 11, 15], 9));
--- 0167_TwoSumII.js
+++ 0167_TwoSumII.js
@@ -1,23 +1,75 @@
 /**
  * @param {number[]} numbers
  * @param {number} target
- * @return {number[]}
+ * @return {number[]}}
  */
 
-/*
-https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
-解法:
-1. 
-*/
+// 解法：
+// 宣告頭為最前面的索引
+// 尾巴為最後面的索引
+// 從 0 遍歷到尾巴
+// 　　將總和宣告為頭值+尾值
+// 　　如果相等於 target
+// 　　　　就回傳頭+1 尾+1 (比照題目需求)
+// 　　如果總和大於目標
+// 　　　　表示其中一個數字太大，以已排序陣列來看必定是尾，所以尾向前走
+//　　　　 i 需要扣掉 1，因為這邊把 i 當成了頭
+// 　　如果總和小於目標
+// 　　　　反推上一個條件，故頭向後走
+
+// var twoSum = function (numbers, target) {
+//   let head = 0;
+//   let tail = numbers.length - 1;
+//   for (let i = 0; i < tail; i++) {
+//     let sum = numbers[head] + numbers[tail];
+//     if (sum === target) {
+//       return [head + 1, tail + 1];
+//     } else if (sum > target) {
+//       tail -= 1;
+//       i--;
+//     } else {
+//       head += 1;
+//     }
+//   }
+// };
+
+// 解法：
+// 宣告頭為最前面的索引
+// 尾巴為最後面的索引
+// 當頭不等於尾時持續進行
+// 　　將總和宣告為頭值+尾值
+// 　　如果相等於 target
+// 　　　　就回傳頭+1 尾+1 (比照題目需求)
+// 　　如果總和大於目標
+// 　　　　表示其中一個數字太大，以已排序陣列來看必定是尾，所以尾向前走
+// 　　如果總和小於目標
+// 　　　　反推上一個條件，故頭向後走
 var twoSum = function (numbers, target) {
-  for (let i = 0; i < numbers.length; i++) {
-    for (let j = i + 1; j < numbers.length; j++) {
-      if (numbers[i] + numbers[j] === target) {
-        return [i + 1, j + 1]
-      }
+  let head = 0;
+  let tail = numbers.length - 1;
+  while (head !== tail) {
+    let sum = numbers[head] + numbers[tail];
+    if (sum === target) {
+      return [head + 1, tail + 1];
+    } else if (sum > target) {
+      tail -= 1;
+    } else {
+      head += 1;
     }
   }
 };
 
+// var twoSum = function (numbers, target) {
+//   for (let i = 0; i < numbers.length; i++) {
+//     for (let j = i + 1; j < numbers.length; j++) {
+//       if (numbers[i] + numbers[j] === target) {
+//         return [i + 1, j + 1];
+//       }
+//     }
+//   }
+// };
 
-console.log(twoSum([2, 3, 4], 6)) // [1,3]
+console.log(twoSum([2, 3, 4], 6)); // [1,3]
+console.log(twoSum([2, 7, 11, 15], 9)); // [1,3]
+console.log(twoSum([-1, 0], -1)); // [1,3]
+console.log(twoSum([5, 25, 75], 100));
