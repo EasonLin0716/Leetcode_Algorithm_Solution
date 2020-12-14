4865b627f3f615121d47c272712a7b1a55d6c34b
Eason Lin <uu9924079@gmail.com>
2020-12-14 22:35:27 +0800

    add 0414 second solution

--- 0414_ThirdMaximumNumber.js
+++ 0414_ThirdMaximumNumber.js
@@ -21,43 +21,42 @@ https://leetcode.com/problems/third-maximum-number/
 //   return nums[2];
 // };
 
-/**
- * 排序>取出不重複數字>回傳第三大
- */
+// 思路：
+// 宣告 a, b, c 為負無限大
+// 遍歷 nums
+// 　　如果被遍歷到的值等於 a, b 或 c
+// 　　　　跳過此次迴圈
+// 　　如果被遍歷到的值大於 a
+// 　　　　b 給 c，a 給 b，遍歷值給 a
+// 　　如果被遍歷到的值大於 b
+// 　　　　b 給 c，遍歷值給 b
+// 　　如果被遍歷到的值大於 c
+// 　　　　遍歷值給 c
+// 如果 c 等於負無限大，就回傳 a 否則回傳 c
+// 我原本的寫法一直卡在重複數字時一樣會進判定，導致陣列未排序時在部分輸入會錯誤的問題，這個解法運用 continue 解決了重複數字的問題，覺得非常妙（也算是第一次看到 continue 這個東西），記錄在這邊
+
 var thirdMax = function (nums) {
-  nums = filterSameNumInArray(quickSort(nums));
-  if (nums.length <= 2) return nums[0];
-  return nums[2];
+  let [a, b, c] = [-Infinity, -Infinity, -Infinity];
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === a || nums[i] === b || nums[i] === c) {
+      continue;
+    }
+    if (nums[i] > a) {
+      c = b;
+      b = a;
+      a = nums[i];
+    } else if (nums[i] > b) {
+      c = b;
+      b = nums[i];
+    } else if (nums[i] > c) {
+      c = nums[i];
+    }
+  }
+  return c === -Infinity ? a : c;
 };
-
-function quickSort(arr) {
-  if (arr.length <= 1) return arr;
-  const pivotIndex = Math.floor(arr.length / 2);
-  // 取得中間值並將其取出
-  const pivot = arr.splice(pivotIndex, 1)[0];
-  const [left, right] = [[], []];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > pivot) {
-      left.push(arr[i]);
-    }
-    if (arr[i] < pivot) {
-      right.push(arr[i]);
-    }
-  }
-  return quickSort(left).concat([pivot], quickSort(right));
-}
-
-function filterSameNumInArray(arr) {
-  if (arr.length <= 1) return arr;
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] === arr[i + 1]) {
-      arr.splice(i, 1);
-      i--;
-    }
-  }
-  return arr;
-}
 
 console.log(thirdMax([3, 2, 1])); // expected output: 1
 console.log(thirdMax([1, 2])); // expected output: 1
 console.log(thirdMax([2, 2, 3, 1])); // expected output: 1
+console.log(thirdMax([5, 2, 2])); // 5
+console.log(thirdMax([1, 2, 2, 5, 3, 5])); // 2
