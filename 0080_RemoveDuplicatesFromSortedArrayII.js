b50e4ad0ebc664c0992f4b9027ba98c16ef9c201
Eason Lin <uu9924079@gmail.com>
2020-12-21 10:27:39 +0800

    add 0080

--- 0080_RemoveDuplicatesFromSortedArrayII.js
+++ 0080_RemoveDuplicatesFromSortedArrayII.js
@@ -12,5 +12,12 @@ var removeDuplicates = function (nums) {
   return nums.length;
 };
 
+// 思路：
+// 遍歷 nums
+//   如果 nums[i] 前前個等於 nums
+//     就把他從陣列中移除，並把索引 -= 1
+// 回傳 nums.length
+// 80ms, 40.5MB
+
 console.log(removeDuplicates([1, 1, 1, 2, 2, 3])); // 5
 console.log(removeDuplicates([0, 0, 1, 1, 2, 3, 3])); // 7
