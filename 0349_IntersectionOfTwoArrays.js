17a67f3ffe1da4c98a424c812665751b71a07f22
Eason Lin <uu9924079@gmail.com>
2021-01-05 21:48:26 +0800

    add second solution

--- 0349_IntersectionOfTwoArrays.js
+++ 0349_IntersectionOfTwoArrays.js
@@ -4,12 +4,26 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
+// var intersection = function (nums1, nums2) {
+//   const ary = [];
+//   for (let i = 0; i < nums1.length; i++) {
+//     if (nums2.includes(nums1[i]) && !ary.includes(nums1[i])) {
+//       ary.push(nums1[i]);
+//     }
+//   }
+//   return ary;
+// };
+
 var intersection = function (nums1, nums2) {
-  const ary = [];
-  for (let i = 0; i < nums1.length; i++) {
-    if (nums2.includes(nums1[i]) && !ary.includes(nums1[i])) {
-      ary.push(nums1[i]);
-    }
+  let map = new Map();
+  for (let num of nums1) {
+    if (!map.has(num)) map.set(num, 1);
   }
-  return ary;
+
+  return nums2.filter((n) => {
+    if (map.has(n)) {
+      map.delete(n);
+      return true;
+    } else return false;
+  });
 };
