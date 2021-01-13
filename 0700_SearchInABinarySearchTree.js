ef5c3be480df95a1640f276cc16b27ec1f12c020
Eason Lin <eason@ecfit.com.tw>
2021-01-13 21:16:18 +0800

    add 0700 (not finished)

---
+++ 0700_SearchInABinarySearchTree.js
@@ -0,0 +1,39 @@
+// You are given the root of a binary search tree (BST) and an integer val.
+
+// Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function (root, val) {
+  return root;
+};
+
+var a = new TreeNode(4);
+var b = new TreeNode(2);
+var c = new TreeNode(1);
+var d = new TreeNode(3);
+var e = new TreeNode(7);
+
+a.left = b;
+b.left = c;
+c.right = d;
+a.right = e;
+
+console.log(searchBST(a, 2));
+
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
