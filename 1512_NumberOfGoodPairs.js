e1e1bbb8c21239e9e9d523c17f99fbd5f8fc05ca
Eason Lin <eason_lin@addcn.com>
2021-12-07 18:56:47 +0800

    add 1512 O(n)

--- 1512_NumberOfGoodPairs.js
+++ 1512_NumberOfGoodPairs.js
@@ -2,12 +2,28 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var numIdenticalPairs = function (nums) {
+//   var res = 0;
+//   for (var i = 0; i < nums.length; i++) {
+//     for (var j = i + 1; j < nums.length; j++) {
+//       if (nums[i] === nums[j] && i < j) res++;
+//     }
+//   }
+//   return res;
+// };
+
+// O(n)
 var numIdenticalPairs = function (nums) {
-  var res = 0;
-  for (var i = 0; i < nums.length; i++) {
-    for (var j = i + 1; j < nums.length; j++) {
-      if (nums[i] === nums[j] && i < j) res++;
+    var res = 0;
+    var map = {};
+    for (var i = 0; i < nums.length; i++) {
+        var num = nums[i]
+        if (map[num] !== undefined) {
+            map[num]++;
+            res += map[num];
+        } else {
+            map[num] = 0;
+        }
     }
-  }
-  return res;
+    return res;
 };
