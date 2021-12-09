00f323496dbab003da238cff7557763adef71db9
Eason Lin <eason_lin@addcn.com>
2021-12-09 08:44:01 +0800

    add 1603

---
+++ 1603_DesignParkingSystem.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} big
+ * @param {number} medium
+ * @param {number} small
+ */
+ var ParkingSystem = function(big, medium, small) {
+    this.ary = [big, medium, small]
+};
+
+/** 
+ * @param {number} carType
+ * @return {boolean}
+ */
+ParkingSystem.prototype.addCar = function(carType) {
+    var idx = carType - 1
+    if (this.ary[idx] > 0) {
+        this.ary[idx] -= 1
+        return true
+    } return false
+};
+
+/** 
+ * Your ParkingSystem object will be instantiated and called as such:
+ * var obj = new ParkingSystem(big, medium, small)
+ * var param_1 = obj.addCar(carType)
+ */
