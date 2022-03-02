9ae03476ce9fd77ab376ab75d9e495205b48d6c0
Eason Lin <uu9924079@gmail.com>
2022-03-02 18:54:42 +0800

    add 0392

--- 0392_IsSubsequence.js
+++ 0392_IsSubsequence.js
@@ -3,20 +3,34 @@
  * @param {string} t
  * @return {boolean}
  */
- var isSubsequence = function(s, t) {
-    // 雙指針
-    if (t.length < s.length) return false;
-    var sLen = s.length; var tLen = t.length;
-    var sPointer = 0; var tPointer = 0;
-    while (sPointer < sLen && tPointer < tLen) {
-        var char = t.charAt(tPointer);
-        var target = s.charAt(sPointer);
-        if (char === target) {
-            sPointer++;
-            tPointer++;
-        } else {
-            tPointer++;
-        }
+var isSubsequence = function (s, t) {
+  // 雙指針
+  if (t.length < s.length) return false;
+  var sLen = s.length;
+  var tLen = t.length;
+  var sPointer = 0;
+  var tPointer = 0;
+  while (sPointer < sLen) {
+    var target = s[sPointer];
+    var char = t[tPointer];
+    if (target === char) {
+      tPointer++;
+      sPointer++;
+    } else {
+      tPointer++;
     }
-    return true;
+    if (tPointer > tLen) return false;
+  }
+  return true;
 };
+
+// Example 1:
+
+// Input: s = "abc", t = "ahbgdc"
+// Output: true
+console.log(isSubsequence("abc", "ahbgdc")); //true
+// Example 2:
+
+// Input: s = "axc", t = "ahbgdc"
+// Output: false
+console.log(isSubsequence("axc", "ahbgdc")); //false
