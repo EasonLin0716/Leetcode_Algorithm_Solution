58213b7660be956386f0686a69314b9a1e3884f0
Eason Lin <uu9924079@gmail.com>
2022-04-14 19:22:59 +0800

    add 1029 comment

--- 1029_TwoCityScheduling.js
+++ 1029_TwoCityScheduling.js
@@ -5,19 +5,24 @@
 var twoCitySchedCost = function (costs) {
   var minusAndIndex = [];
   for (var i = 0; i < costs.length; i++) {
+    // 這邊會存 [[AB的差, 索引], ...]
     minusAndIndex.push([costs[i][0] - costs[i][1], i]);
   }
+  // 根據 AB 的差作排序 中間以左是選A更划算 中間以右是選B更划算
   minusAndIndex.sort((a, b) => a[0] - b[0]);
-  var first = 0,
-    last = costs.length - 1;
+  var left = 0,
+    right = costs.length - 1;
   var sum = 0;
-  while (first < last) {
-    var firstIndex = minusAndIndex[first][1];
-    var lastIndex = minusAndIndex[last][1];
+  while (left < right) {
+    // 去抓出 costs 索引
+    var firstIndex = minusAndIndex[left][1];
+    var lastIndex = minusAndIndex[right][1];
+    // 左邊選A
     sum += costs[firstIndex][0];
+    // 右邊選B
     sum += costs[lastIndex][1];
-    first++;
-    last--;
+    left++;
+    right--;
   }
   return sum;
 };
